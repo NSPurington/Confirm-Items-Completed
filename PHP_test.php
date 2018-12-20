04f8970b8ff241f0966304947fdcc53dd9251759
Nicholas Purington <nickpurington@gmail.com>
2018-12-20 13:27:20 -0800

    Add PHP_test.php file

---
+++ PHP_test.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<?php  
+
+echo "<h2>Training Quiz</h2>";
+
+echo "<a href='<?php echo myfunction(); ?>'>xyz</a>";
+
+<?php 
+function myfunction() 
+{ 
+echo "http://google.com"; 
+} 
+
+?>
+
+</body>
+</html>
